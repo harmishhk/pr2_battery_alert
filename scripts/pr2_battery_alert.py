dfa478b67b5c731f84488c8a1ede6459109b7da8
harmish khambhaita <harmish@laas.fr>
2016-11-21 23:22:51 +0100

    lower volume and frequncy for low level tab = 4 spaces in editorconfig

--- scripts/pr2_battery_alert.py
+++ scripts/pr2_battery_alert.py
@@ -13,6 +13,7 @@ class Pr2BatteryAlert:
     def main(self):
         self.last_relative_capacity = 100
         self.last_AC_present = 16
+        self.low_counter = 0
 
         rospy.init_node('pr2_battery_alert')
 
@@ -22,22 +23,29 @@ class Pr2BatteryAlert:
 
         sound_request = SoundRequest()
         sound_request.command = 1
+        sound_request.sound = 3
         rate = rospy.Rate(0.2)  # every 5 second
         while not rospy.is_shutdown():
             if self.last_AC_present == 0:
                 if self.last_relative_capacity <= CRITICAL_LEVEL:
                     rospy.logdebug(
                         "Battery critically low, level %d", self.last_relative_capacity)
-                    sound_request.sound = 5
+                    sound_request.volume = 1.0
+                    self.low_counter = 0
                     pub.publish(sound_request)
                 elif self.last_relative_capacity <= LOW_LEVEL:
-                    rospy.logdebug(
-                        "Battery low, level %d", self.last_relative_capacity)
-                    sound_request.sound = 3
-                    pub.publish(sound_request)
+                    if self.low_counter % 6 == 0:
+                        rospy.logdebug(
+                            "Battery low, level %d", self.last_relative_capacity)
+                        sound_request.volume = 0.1
+                        pub.publish(sound_request)
+                    self.low_counter += 1
                 else:
                     rospy.logdebug(
                         "Battery OK, level %d", self.last_relative_capacity)
+                    self.low_counter = 0
+            else:
+                self.low_counter = 0
             rate.sleep()
 
     def callback(self, data):
